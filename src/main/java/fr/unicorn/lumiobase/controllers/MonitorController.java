ee7dcbf2883da09504e73726a84590fc6165c08b
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-01-19 22:48:38 +0100

    Finished Monitor Template

--- src/main/java/fr/unicorn/lumiobase/controllers/MonitorController.java
+++ src/main/java/fr/unicorn/lumiobase/controllers/MonitorController.java
@@ -1,6 +1,7 @@
 package fr.unicorn.lumiobase.controllers;
 
 import fr.unicorn.lumiobase.SpringApp;
+import fr.unicorn.lumiobase.sensors.remote.RemoteValues;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class MonitorController {
         model.addAttribute("abshumidity", SpringApp.abshumidityHistory);
         model.addAttribute("presence", SpringApp.presenceHistory);
         model.addAttribute("distance", SpringApp.distanceHistory);
+        model.addAttribute("remote", RemoteValues.POWER.toString());
         return "monitor";
     }
 
