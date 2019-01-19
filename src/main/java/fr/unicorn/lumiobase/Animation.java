53b9dd424c881fc92bcd50e02165bb1cf867351c
Unknown <victor.morgant@gmail.com>
2019-01-19 21:02:44 +0100

    Changing animation

--- src/main/java/fr/unicorn/lumiobase/Animation.java
+++ src/main/java/fr/unicorn/lumiobase/Animation.java
@@ -106,13 +106,11 @@ public class Animation {
         }
     }
 
-    public static boolean arrow(Color color, int time, int delay, String id_laumio) throws NameAlreadyUsedException {
-        int compt = 0;
-        int pix1[]={0,0};
-        int pix2[]={1,1};
-        int pix3[]={0,2};
-        while(compt<time){
-            compt+=delay;
+    public static boolean arrow(Color color, int delay, String id_laumio) throws NameAlreadyUsedException {
+        int pix1=6;
+        int pix2=0;
+        int pix3=12;
+        while(pix2!=3){
             try {
                 Thread.sleep(delay);
             } catch (InterruptedException e) {
@@ -120,17 +118,17 @@ public class Animation {
                 return false;
             }
             Boubou.TurnOffLumio(id_laumio);
-            ColorPixel.SendColorPixel(color,Util.getIdPixel(pix1[0],pix1[1]),id_laumio);
-            ColorPixel.SendColorPixel(color,Util.getIdPixel(pix2[0],pix2[1]),id_laumio);
-            ColorPixel.SendColorPixel(color,Util.getIdPixel(pix3[0],pix3[1]),id_laumio);
-            pix1[1]++;
-            pix2[1]++;
-            pix3[1]++;
+            ColorPixel.SendColorPixel(color,pix1,id_laumio);
+            ColorPixel.SendColorPixel(color,pix2,id_laumio);
+            ColorPixel.SendColorPixel(color,pix3,id_laumio);
+            pix1++;
+            pix2++;
+            pix3--;
         }
         return true;
     }
-    public static void arrow(Color color, int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
-        for (String s : idsLaumio) arrow(color,time,delay,s);
+    public static void arrow(Color color, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
+        for (String s : idsLaumio) arrow(color,delay,s);
     }
 
     public static boolean plus(int time, int delay) throws NameAlreadyUsedException {
@@ -209,29 +207,26 @@ public class Animation {
         for (String s : idsLaumio) minus(time,delay,s);
     }
 
-    public static boolean rain(int time, int delay, String id_laumio) throws NameAlreadyUsedException {
+    public static boolean rain(int time, int delay, String id_laumio) throws NameAlreadyUsedException,InterruptedException  {
         Color blue=Color.create("Blue",0,0,255);
         Color white=Color.create("Blue",255,255,255);
         int compt = 0;
         while(compt<time) {
             compt += delay;
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return false;
-            }
             Boubou.TurnOffLumio(id_laumio);
-            ColorRing.sendColorRing(white, 0);
-            ColorPixel.SendColorPixel(blue,Util.getIdPixel(1,1),id_laumio);
-            ColorPixel.SendColorPixel(blue,Util.getIdPixel(1,2),id_laumio);
-            ColorPixel.SendColorPixel(blue,Util.getIdPixel(2,1),id_laumio);
-            ColorPixel.SendColorPixel(blue,Util.getIdPixel(2,2),id_laumio);
+            ColorRing.sendColorRing(white, 2);
+            ColorRing.sendColorRing(blue, 1);
+            Thread.sleep(delay);
+            ColorRing.sendColorRing(blue, 0);
+            Thread.sleep(delay);
+            ColorRing.sendColorRing(white,0);
+            Thread.sleep(delay);
+            ColorRing.sendColorRing(white,1);
         }
         return true;
     }
 
-    public static void rain(int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
+    public static void rain(int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException, InterruptedException {
         for (String s : idsLaumio) rain(time,delay,s);
     }
 
@@ -247,10 +242,11 @@ public class Animation {
                 return false;
             }
             Boubou.TurnOffLumio(id_laumio);
-            ColorPixel.SendColorPixel(green,Util.getIdPixel(1,0),id_laumio);
-            ColorPixel.SendColorPixel(green,Util.getIdPixel(2,1),id_laumio);
-            ColorPixel.SendColorPixel(green,Util.getIdPixel(2,2),id_laumio);
-            ColorPixel.SendColorPixel(green,Util.getIdPixel(1,3),id_laumio);
+            ColorPixel.SendColorPixel(green,2,id_laumio);
+            ColorPixel.SendColorPixel(green,3,id_laumio);
+            ColorPixel.SendColorPixel(green,1,id_laumio);
+            ColorPixel.SendColorPixel(green,12,id_laumio);
+            ColorPixel.SendColorPixel(green,5,id_laumio);
 
         }
         return true;
@@ -272,10 +268,11 @@ public class Animation {
                 return false;
             }
             Boubou.TurnOffLumio(id_laumio);
-            ColorPixel.SendColorPixel(red,Util.getIdPixel(0,2),id_laumio);
-            ColorPixel.SendColorPixel(red,Util.getIdPixel(1,1),id_laumio);
-            ColorPixel.SendColorPixel(red,Util.getIdPixel(1,2),id_laumio);
-            ColorPixel.SendColorPixel(red,Util.getIdPixel(2,3),id_laumio);
+            ColorPixel.SendColorPixel(red,2,id_laumio);
+            ColorPixel.SendColorPixel(red,3,id_laumio);
+            ColorPixel.SendColorPixel(red,0,id_laumio);
+            ColorPixel.SendColorPixel(red,11,id_laumio);
+            ColorPixel.SendColorPixel(red,5,id_laumio);
 
         }
         return true;
@@ -285,4 +282,21 @@ public class Animation {
         for (String s : idsLaumio) sad(time,delay,s);
     }
 
+    public static boolean sequence(Color c,int nb_led,int delay, String id_laumio) throws NameAlreadyUsedException {
+        Boubou.TurnOffLumio(id_laumio);
+        for(int compte=0;compte<nb_led;compte++) {
+            ColorPixel.SendColorPixel(c,compte,id_laumio);
+            if(compte>1) {
+                ColorPixel.TurnOffLights(compte - 1, id_laumio);
+            }
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+    
 }
