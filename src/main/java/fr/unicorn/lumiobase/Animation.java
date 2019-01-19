30f6672bf8278904dfee20c1fd03fb7358e23ff7
Unknown <victor.morgant@gmail.com>
2019-01-19 19:18:25 +0100

    adding method for group and id animation

--- src/main/java/fr/unicorn/lumiobase/Animation.java
+++ src/main/java/fr/unicorn/lumiobase/Animation.java
@@ -3,6 +3,7 @@ package fr.unicorn.lumiobase;
 import org.eclipse.paho.client.mqttv3.*;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Animation {
@@ -34,6 +35,23 @@ public class Animation {
         return true;
     }
 
+    public static boolean verticalWipe(Color rvb, int delay, String idLaumio){
+        for(int line=0 ; line < ReadProperties.prop.getJSONObject("lumio").getInt("NB_LINE") ; line++){
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+            ColorRing.sendColorRing(rvb, line,idLaumio);
+        }
+        return true;
+    }
+
+    public static void verticalWipe(Color rvb, int delay, ArrayList<String> idsLaumio){
+        for (String s : idsLaumio) verticalWipe(rvb, delay, s);
+    }
+
     public static boolean rainbow(){
 
         IMqttClient publisher = Connections.connectPublisher();
@@ -50,6 +68,28 @@ public class Animation {
         }
         return true;
     }
+
+    public static boolean rainbow(String idLaumio){
+
+        IMqttClient publisher = Connections.connectPublisher();
+
+        JSONObject json = new JSONObject();
+        json.put("command", "animate_rainbow");
+        MqttMessage message = new MqttMessage();
+        message.setPayload(json.toString().getBytes());
+        try {
+            publisher.publish("laumio/"+idLaumio+"/json",message);
+        } catch (MqttException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static void rainbow(ArrayList<String> idsLaumio){
+        for (String s : idsLaumio) rainbow(s);
+    }
+
 
     public static boolean glow(Color color, int time, int delay){
         int compt = 0;
@@ -76,6 +116,37 @@ public class Animation {
         }
         return true;
     }
+
+    public static boolean glow(Color color, int time, int delay, String idLaumio){
+        int compt = 0;
+        boolean reduce = true;
+        while(compt<time){
+            compt+=delay;
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+            Boubou.SendColorLumio(color,idLaumio);
+
+            if(reduce){
+                if(!color.reduce()){
+                    reduce = false;
+                }
+            }else{
+                if(!color.increase()){
+                    reduce = true;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void glow(Color color, int time, int delay, ArrayList<String> idsLaumio){
+        for (String s : idsLaumio) glow(color,time,delay,s);
+    }
+
     public static boolean arrow(Color color, int time, int delay, String id_laumio) throws NameAlreadyUsedException {
         int compt = 0;
         int pix1[]={0,0};
@@ -99,6 +170,9 @@ public class Animation {
         }
         return true;
     }
+    public static void arrow(Color color, int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
+        for (String s : idsLaumio) arrow(color,time,delay,s);
+    }
 
     public static boolean plus(int time, int delay) throws NameAlreadyUsedException {
         Color green=Color.create("Green",0,255,0);
@@ -118,6 +192,28 @@ public class Animation {
         return true;
     }
 
+    public static boolean plus(int time, int delay, String idLaumio) throws NameAlreadyUsedException {
+        Color green=Color.create("Green",0,255,0);
+        int compt = 0;
+        while(compt<time) {
+            compt += delay;
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+            ColorRing.sendColorRing(green, 1,idLaumio);
+            ColorColumn.SendColorColumn(green,1,idLaumio);
+            ColorColumn.SendColorColumn(green,2,idLaumio);
+        }
+        return true;
+    }
+
+    public static void plus(int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
+        for (String s : idsLaumio) plus(time,delay,s);
+    }
+
     public static boolean minus(int time, int delay ) throws NameAlreadyUsedException {
         Color red=Color.create("Red",255,0,0);
         int compt = 0;
@@ -132,6 +228,26 @@ public class Animation {
             ColorRing.sendColorRing(red, 1);
         }
         return true;
+    }
+
+    public static boolean minus(int time, int delay, String idLaumio ) throws NameAlreadyUsedException {
+        Color red=Color.create("Red",255,0,0);
+        int compt = 0;
+        while(compt<time) {
+            compt += delay;
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+            ColorRing.sendColorRing(red, 1,idLaumio);
+        }
+        return true;
+    }
+
+    public static void minus(int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
+        for (String s : idsLaumio) minus(time,delay,s);
     }
 
     public static boolean rain(int time, int delay, String id_laumio) throws NameAlreadyUsedException {
@@ -156,6 +272,10 @@ public class Animation {
         return true;
     }
 
+    public static void rain(int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
+        for (String s : idsLaumio) rain(time,delay,s);
+    }
+
     public static boolean happy(int time, int delay, String id_laumio) throws NameAlreadyUsedException {
         Color green=Color.create("Green",0,255,0);
         int compt = 0;
@@ -175,6 +295,10 @@ public class Animation {
 
         }
         return true;
+    }
+
+    public static void happy(int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
+        for (String s : idsLaumio) happy(time,delay,s);
     }
 
     public static boolean sad(int time, int delay, String id_laumio) throws NameAlreadyUsedException {
@@ -198,5 +322,8 @@ public class Animation {
         return true;
     }
 
+    public static void sad(int time, int delay, ArrayList<String> idsLaumio) throws NameAlreadyUsedException {
+        for (String s : idsLaumio) sad(time,delay,s);
+    }
 
 }
