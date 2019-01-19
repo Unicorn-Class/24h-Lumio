7c9f7ccc5bc860b3fcd5fbd7e94761a15a2396ae
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-01-19 14:46:13 +0100

    Updated & Finished ColorPixel

--- src/main/java/fr/unicorn/lumiobase/ColorPixel.java
+++ src/main/java/fr/unicorn/lumiobase/ColorPixel.java
@@ -3,14 +3,16 @@ package fr.unicorn.lumiobase;
 import org.eclipse.paho.client.mqttv3.*;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class ColorPixel {
 
     public static void main(String args[]) throws NameAlreadyUsedException, InterruptedException {
         SendColorPixelDuring(Color.create("Bizarre",255,255,255),2,"all",2000);
-        return;
     }
 
-    public static boolean SendColorPixel(Color c, int idPixel, String idLumio){
+
+    public static void SendColorPixel(Color c, int idPixel, String idLumio){
         IMqttClient publisher = Connections.connectPublisher(false);
         JSONObject json = new JSONObject();
         json.put("command", "set_pixel");
@@ -22,13 +24,19 @@ public class ColorPixel {
             publisher.publish("laumio/"+idLumio+"/json",message);
         } catch (MqttException e) {
             e.printStackTrace();
-            return false;
         }
-        return true;
+    }
+
+    public static void SendColorPixel(Color c, int idPixel, ArrayList<String> idsLumio) {
+        for (String s : idsLumio) SendColorPixel(c, idPixel, s);
     }
 
     public static void TurnOffLights(int idPixel, String idLumio) throws NameAlreadyUsedException {
         SendColorPixel(Color.create("Off",0,0,0),idPixel,idLumio);
+    }
+
+    public static void TurnOffLights(int idPixel, ArrayList<String> idsLumio) throws NameAlreadyUsedException {
+        for (String s : idsLumio) SendColorPixel(Color.create("Off",0,0,0),idPixel,s);
     }
 
     public static void SendColorPixelDuring(Color c, int idPixel, String idLumio, int duration) throws InterruptedException, NameAlreadyUsedException {
