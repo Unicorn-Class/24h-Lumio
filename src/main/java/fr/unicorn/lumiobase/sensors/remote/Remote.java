05e4b03455f7003c1a4e1214e347c2d066c0ba27
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-01-19 15:11:59 +0100

    Added Remote Support

---
+++ src/main/java/fr/unicorn/lumiobase/sensors/remote/Remote.java
@@ -0,0 +1,123 @@
+package fr.unicorn.lumiobase.sensors.remote;
+
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class Remote{
+
+    public static void initRemote(IMqttClient client) throws MqttException {
+        client.subscribe("remote/power/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.POWER);
+        });
+        client.subscribe("remote/mode/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.MODE);
+        });
+        client.subscribe("remote/mute/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.MUTE);
+        });
+        client.subscribe("remote/playp/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.PLAYP);
+        });
+        client.subscribe("remote/prev/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.PREV);
+        });
+        client.subscribe("remote/next/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NEXT);
+        });
+        client.subscribe("remote/eq/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.EQ);
+        });
+        client.subscribe("remote/minus/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.MINUS);
+        });
+        client.subscribe("remote/plus/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.PLUS);
+        });
+        client.subscribe("remote/chg/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.CHG);
+        });
+        client.subscribe("remote/u_sd/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.U_SD);
+        });
+        client.subscribe("remote/0/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_0);
+        });
+        client.subscribe("remote/1/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_1);
+        });
+        client.subscribe("remote/2/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_2);
+        });
+        client.subscribe("remote/3/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_3);
+        });
+        client.subscribe("remote/4/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_4);
+        });
+        client.subscribe("remote/5/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_5);
+        });
+        client.subscribe("remote/6/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_6);
+        });
+        client.subscribe("remote/7/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_7);
+        });
+        client.subscribe("remote/8/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_8);
+        });
+        client.subscribe("remote/9/state", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String str = new String(payload);
+            if (str.equals("true")) btnPushed(RemoteValues.NB_9);
+        });
+
+    }
+
+    public static void btnPushed(RemoteValues val) {
+        switch (val) {
+
+        }
+    }
+
+}
---
+++ src/main/java/fr/unicorn/lumiobase/sensors/remote/RemoteValues.java
@@ -0,0 +1,5 @@
+package fr.unicorn.lumiobase.sensors.remote;
+
+public enum RemoteValues {
+    POWER, MODE, MUTE, PLAYP, PREV, NEXT, EQ, MINUS, PLUS, CHG, U_SD, NB_0, NB_1, NB_2, NB_3, NB_4, NB_5, NB_6, NB_7, NB_8, NB_9
+}
