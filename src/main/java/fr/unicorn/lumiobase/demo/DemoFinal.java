79f657d202ba540da2c819a20168b62570be249e
Unknown <victor.morgant@gmail.com>
2019-01-20 05:13:40 +0100

    Adding template html for Meteo+mood+plan for final demo

---
+++ src/main/java/fr/unicorn/lumiobase/demo/DemoFinal.java
@@ -0,0 +1,18 @@
+package fr.unicorn.lumiobase.demo;
+
+public class DemoFinal {
+
+        /**Meteo**/
+
+        /**Monitor**/
+
+        /**Remote**/
+
+        /**Mood**/
+
+        /**Morpion**/
+
+        /**Memo**/
+
+        /**Animation**/
+}
