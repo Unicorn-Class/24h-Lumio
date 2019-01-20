2f83edf2c98c5b8dc1eb0ac71b50f0539b58b54a
Elodie Morin <Elodie.morin1298@gmail.com>
2019-01-20 02:51:06 +0100

    Div sale

--- src/main/java/fr/unicorn/lumiobase/sensors/CapteurPushButton.java
+++ src/main/java/fr/unicorn/lumiobase/sensors/CapteurPushButton.java
@@ -1,5 +1,6 @@
 package fr.unicorn.lumiobase.sensors;
 
+import fr.unicorn.lumiobase.Color;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
@@ -36,12 +37,12 @@ public class CapteurPushButton {
             System.out.println(str);
         });
 
-   /*     client.subscribe("capteur_bp/switch/led" + num + "/state", (topic, msg) -> {
+        client.subscribe("capteur_bp/switch/led" + num + "/state", (topic, msg) -> {
             byte[] payload = msg.getPayload();
             String str = new String(payload);
             ledState[num] = (str == "ON") ? true : false;
             System.out.println(str);
-        });*/
+        });
 
         client.subscribe("capteur_bp/binary_sensor/bp" + num + "/state", (topic, msg) -> {
             byte[] payload = msg.getPayload();
